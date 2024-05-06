86612d0d9348e2b88e88a7e2f5b3cf563c261c2d
bigsteppa05 <habibforderiv05@gmail.com>
2024-05-06 09:17:14 +0300

    will redo later on

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,27 @@
-
+import React from 'react';
+import { Home, Movie, Actors, Directors, ErrorPage } from './pages';
 
 const routes = [
-   
-  ];
+  {
+    path: '/',
+    Component: Home,
+  },
+  {
+    path: '/movie/:id',
+    Component: Movie,
+  },
+  {
+    path: '/actors',
+    Component: Actors,
+  },
+  {
+    path: '/directors',
+    Component: Directors,
+  },
+  {
+    Component: ErrorPage,
+  },
+];
 
 export default routes;
+
